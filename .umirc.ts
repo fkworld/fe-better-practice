0e0249c8ed08c726f84bf01b6b8e5327faea919d
-g <fkworld@foxmail.com>
2021-01-11 14:49:13 +0800

    feat: init project

---
+++ .umirc.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'fe-better-practice',
+  description: '基于 React 的前端项目最佳实践探索',
+  mode: 'doc',
+  locales: [['zh-CN', '中文']],
+  esbuild: {},
+});
