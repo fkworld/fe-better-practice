391ae6fea2cf0a3e08c6e8ee9ca77805e11415df
-g <fkworld@foxmail.com>
2021-01-11 15:18:45 +0800

    chore: 根据github-page要求修改配置信息

--- .umirc.ts
+++ .umirc.ts
@@ -4,6 +4,13 @@ export default defineConfig({
   title: 'fe-better-practice',
   description: '基于 React 的前端项目最佳实践探索',
   mode: 'doc',
+  // Github Pages 需要使用 docs 作为输出目录，对应的 resolve/includes 也需要修改
+  outputPath: 'docs',
+  resolve: {
+    includes: ['src'],
+  },
+  // 默认仅使用中文
   locales: [['zh-CN', '中文']],
+  // 使用 esbuild 打包
   esbuild: {},
 });
